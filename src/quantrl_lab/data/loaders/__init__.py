c70fc6ab729eb85b87540b96b72a76beb6bd6cc5
whanyu1212 <whanyu1212@hotmail.com>
2025-06-20 11:08:14 +0800

    adding data source registry and example notebook

---
+++ src/quantrl_lab/data/loaders/__init__.py
@@ -0,0 +1,5 @@
+from quantrl_lab.data.loaders.alpaca_loader import AlpacaDataLoader  # noqa: F401
+from quantrl_lab.data.loaders.alpha_vantage_loader import (  # noqa: F401
+    AlphaVantageDataLoader,
+)
+from quantrl_lab.data.loaders.yfinance_loader import YfinanceDataloader  # noqa: F401
