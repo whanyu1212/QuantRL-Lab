9319f99d089b30b4e22302a9c9ae60339de27641
whanyu1212 <whanyu1212@hotmail.com>
2025-07-05 13:36:34 +0800

    adding a universal tradingenv protocol to fix the tight coupling in type annotation

--- src/quantrl_lab/custom_envs/core/__init__.py
+++ src/quantrl_lab/custom_envs/core/__init__.py
@@ -1,3 +1,4 @@
 from .actions import Actions  # noqa: F401
 from .config import CoreEnvConfig  # noqa: F401
 from .portfolio import Portfolio  # noqa: F401
+from .trading_env import TradingEnvProtocol  # noqa: F401
