822eec5d8056146780229cbff190e0095f40d076
GiTaDi-CrEaTe <kar323834@gmail.com>
2026-05-19 11:04:01 +0530

    feat: add VolatilityObservationStrategy for RL agents

--- src/quantrl_lab/environments/stock/strategies/observations/__init__.py
+++ src/quantrl_lab/environments/stock/strategies/observations/__init__.py
@@ -3,3 +3,4 @@ from quantrl_lab.environments.core.interfaces import (  # noqa: F401
 )
 
 from .feature_aware import FeatureAwareObservationStrategy  # noqa: F401
+from .volatility_aware import VolatilityObservationStrategy
